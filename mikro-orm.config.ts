f15756493b9a54855dc9ab6660789b39f0819908
Sergio Gragera <sergiogragera@gmail.com>
2025-01-05 10:08:21 +0000

    build: add migration up script

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -2,8 +2,6 @@ import { Migrator } from '@mikro-orm/migrations';
 import { defineConfig } from '@mikro-orm/postgresql';
 
 export default defineConfig({
-  host: process.env.DATABASE_HOST || 'ddd-nestjs-beer-tap-dispenser-db',
-  port: parseInt(process.env.DATABASE_PORT ?? '5432', 10),
   user: process.env.DATABASE_USER || 'postgres',
   password: process.env.DATABASE_PASSWORD || 'postgres',
   dbName: process.env.DATABASE_NAME || 'postgres',
